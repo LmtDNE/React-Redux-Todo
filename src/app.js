7d7676c2527fd347fc6a9e88c3f1ce053efbe9b8
Dion <Dion.Fulwood@gmail.com>
2016-09-28 23:05:22 -0700

    Completes container collection using Provider component

--- src/app.js
+++ src/app.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { TodoList } from './components';
-import { List, Map } from 'immutable';
+import { createStore } from 'react-redux';
+import { Provider } from 'react-redux'; 
 import reducer from './reducer';
+import { TodoList } from './components';
 
 const store = createStore(reducer);
 
 
-const dummyTodos = List([
-  Map({ id: 0, isDone: true, text: 'make components'}),
-  Map({ id: 1, isDone: false, text: 'design actions'}),
-  Map({ id: 2, isDone: false, text: 'implement reducer'}),
-  Map({ id: 3, isDone: false, text: 'connect components'})
-]);
-
 render (
-    <TodoList todos = {store.getState()} />,
+    <Provider store = {store}>
+      <TodoList todos = {store.getState()} />
+    </Provider>,
     document.getElementById('app')
   );
